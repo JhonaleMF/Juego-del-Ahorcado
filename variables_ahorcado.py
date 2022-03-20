f8227d73d24f3b515a2954f877d71c2fdf838d0d
Jhon Alejandro Moncaleano Forero <98182010+JhonaleMF@users.noreply.github.com>
2022-03-20 13:29:34 +0100

    Delete variables_ahorcado.py

--- variables_ahorcado.py
+++
@@ -1,8 +0,0 @@
-message_instructions =("AHORCADO\n"
-                            "1. Al empezar el juego, deberá ingresar el número de intentos a adivinar la palabra. \n"
-                                "2. Ingrese la palabra a adivinar. \n"
-                                "3. Podra ingresar por la teltra o si ya sabes que palabra es, puedes ingresarla en su totalidad\n"
-                                "4. Durante el juego aparecerán mensajes que te indican si ingresaste un valor indebido como por ejemplo:\n"
-                                "\t- Ingresar caracteres especiales\n" 
-                                "\t- Repetir valores ingresados\n" 
-                                "Por último, disfruta del juego.")
